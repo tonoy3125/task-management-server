98f3ed5b1c22051d6ba4c3e6ef36b322917fc7b9
tonoy3125 <shaifshajedt@gmail.com>
2023-12-21 22:58:07 +0600

    task post method added

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-
-app.use(cors());
-app.use(express.json());
-
 const port = 5000;
 
+app.use(cors())
+app.use(express.json())
 
 
 
@@ -27,12 +25,27 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const taskCollection = client.db('taskDB').collection('tasks')
+
+
+
+        app.post('/tasks', async (req, res) => {
+            const task = req.body;
+            const result = await taskCollection.insertOne(task);
+            res.send(result);
+        });
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
